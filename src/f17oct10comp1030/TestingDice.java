04f4485a4e8aa5300573512d8e7d0f0226f509f2
JaretWright <hkkA223>
2017-10-10 16:14:35 -0400

    Added testing Dice code

--- src/f17oct10comp1030/TestingDice.java
+++ src/f17oct10comp1030/TestingDice.java
@@ -15,12 +15,20 @@ public class TestingDice {
         Dice        die1        = new Dice(6);
         Dice        die2        = new Dice(6);
         
-        System.out.printf("Die 1: %d \t Die2: %d%n", die1.getFaceValue(), die2.getFaceValue());
+        //initial count
+        int count = 1;
         
-        die1.rollDie();
-        die2.rollDie();
+        //loop while we do not have snake eyes
+        while (die1.getFaceValue() + die2.getFaceValue() != 2)
+        {
+            System.out.printf("%d.  Die 1: %d \t Die2: %d%n", count, die1.getFaceValue(), die2.getFaceValue());
+            count++;
+            die1.rollDie();
+            die2.rollDie();
+        }
         
-        System.out.printf("Die 1: %d \t Die2: %d%n", die1.getFaceValue(), die2.getFaceValue());
+        System.out.printf("%d.  Die 1: %d \t Die2: %d%n",count, die1.getFaceValue(), die2.getFaceValue());
+        System.out.printf("It took %d attempts to roll snake eyes%n", count);
         
         
     }
